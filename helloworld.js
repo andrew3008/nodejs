34daa0157c5dd17179257f6629e05b476e91e722
Ranjith <nranjithnadar@gmail.com>
2013-12-19 00:53:29 +0530

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function(res, req){
+http.createServer(function(req, res){
  res.writeHead('200',{'Content-type':'text/plain'});
  res.end("Hello World");
 }).listen(8080);
